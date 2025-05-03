0be343b3e228a1a2d5672892d955c242aeee7e8e
Eskimo1976 <karamba1199@gmail.com>
2025-05-03 22:14:29 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,21 @@
 from flask import Flask
-from threading import Thread
+import threading
+import os
 
-app = Flask('')
+# Создаём Flask-приложение
+app = Flask(__name__)
 
+# Обрабатываем корневой маршрут
 @app.route('/')
 def home():
-    return "Бот работает круглосуточно!"
+    return 'Бот работает!'
 
+# Функция запуска Flask-сервера
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 3000))  # Railway передаёт нужный порт сюда
+    app.run(host='0.0.0.0', port=port)
 
+# Фоновый запуск сервера
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    thread = threading.Thread(target=run)
+    thread.start()
