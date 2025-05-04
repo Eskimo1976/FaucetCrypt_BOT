c4a750be03a0e9e52b368a3d090c271dd63c319a
Eskimo1976 <karamba1199@gmail.com>
2025-05-04 08:56:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ USERS_FILE = "users.txt"
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
+user_clicks = {}  # Словарь для отслеживания кликов
+
 @app.route("/")
 def index():
     return "Бот работает!", 200
@@ -27,89 +29,32 @@ def save_user(user_id, username):
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     save_user(message.chat.id, message.from_user.username)
+    user_clicks[message.chat.id] = 0  # сбрасываем счётчик при старте
 
     description = (
         "Добро пожаловать в *FaucetUA!*\n\n"
-        "Ниже представлены лучшие сайты для заработка криптовалюты без вложений.\n"
         "Выберите интересующий ресурс и начните зарабатывать!"
     )
 
-    links = [
-        ("BtcViev", "https://www.bitcoviews.com"),
-        ("FireFaucet", "https://firefaucet.win"),
-        ("Moneta FV", "https://helpfpcoin.site"),
-        ("Link LTC", "https://cflink.pw"),
-        ("Link USDT", "https://cflink.pw"),
-        ("Dogycoin", "https://cflink.pw"),
-        ("Binance", "https://cflink.pw"),
-        ("PEPE", "https://cflink.pw"),
-        ("SolanaCF", "https://cflink.pw"),
-        ("ADTrump", "https://cflink.pw"),
-        ("Links TRX", "https://cflink.pw"),
-        ("LINKS TaRa", "https://cflink.pw"),
-        ("CF Ripple", "https://cflink.pw"),
-        ("Poligon", "https://cflink.pw"),
-        ("OnlyFaucet", "https://onlyfaucet.com"),
-        ("СrYpto", "https://earncryptowrs.in"),
-        ("Gemerle", "https://gamerlee.com"),
-        ("EarnSolana", "https://earnsolana.xyz"),
-        ("CryptoFuture", "https://cryptofuture.co.in"),
-        ("ClaimCrypt", "https://claimcrypto.in"),
-        ("Effa Crypt", "https://eftacrypto.com"),
-        ("Freee Crypt", "https://freeltc.fun"),
-        ("Wheal", "https://wheelofgold.com"),
-        ("FaucetWorld", "https://faucetworld.in"),
-        ("RS Faucet", "https://rsfaucet.com"),
-        ("BestPaying", "https://bestpayingfaucet.online"),
-        ("Earn Crypto", "https://earn-pepe.com"),
-        ("EarnTrump", "https://earn-trump.com"),
-        ("Baggy Cript", "https://bagi.co.in"),
-        ("FaucetPay", "https://faucetpay.io"),
-        ("SatoshiFau", "https://satoshifaucet.io"),
-        ("Ad BTC", "https://r.adbtc.top"),
-        ("Links TON", "https://ton.leaks.work"),
-        ("KiddyEarn", "https://kiddyearner.com"),
-        ("FaucetUsdt", "https://treaw.com"),
-        ("Link Dogy", "https://ch3zo.com"),
-        ("FaucetPayz", "https://faucetpayz.com"),
-        ("Whoopyy", "https://whoopyrewards.com"),
-        ("GenkMinner", "https://genkiminer.com"),
-        ("MultiCoin", "https://assetni.com"),
-        ("SkullBTC", "https://skullbtc.site"),
-        ("SolWin", "https://solwin.site"),
-        ("1doge", "https://1doge.site"),
-        ("CFAIRDROP", "https://cfairdrop.site"),
-        ("EarnEvery", "https://earnevery.site"),
-        ("OnlyEarn", "https://onlyearn.site"),
-        ("WheelPRO", "https://wheelpro.site"),
-        ("USDTwins", "https://usdtwins.site"),
-        # Ссылки со второй страницы сайта (home-1)
-        ("BitcoFarm", "https://bitcofarm.online"),
-        ("TopReward", "https://topreward.online"),
-        ("CoinsFlow", "https://coinsflow.online"),
-        ("LuckyClick", "https://luckyclick.online"),
-        ("FaucetStep", "https://faucetstep.online"),
-        ("DogeWin", "https://dogewin.online"),
-        ("BTCCloud", "https://btccloud.online"),
-        ("QuickCrypto", "https://quickcrypto.online"),
-        ("DailySpin", "https://dailyspin.online"),
-        ("MagicClaim", "https://magicclaim.online"),
-        ("CryptoBox", "https://cryptobox.online"),
-        ("AirClaim", "https://airclaim.online"),
-        ("SpinGold", "https://spingold.online"),
-        ("DigiMine", "https://digimine.online"),
-        ("MoonClicks", "https://moonclicks.online"),
-        ("LinkBits", "https://linkbits.online"),
-    ]
-
-    markup = InlineKeyboardMarkup(row_width=2)
-    for i in range(0, len(links), 2):
-        row = [InlineKeyboardButton(text=name, url=url) for name, url in links[i:i + 2]]
-        markup.add(*row)
-
-    markup.add(InlineKeyboardButton("Перейти на сайт FaucetUA", url="https://faucetua.online/"))
+    markup = InlineKeyboardMarkup(row_width=1)
+    markup.add(
+        InlineKeyboardButton("BtcViev", callback_data="link_clicked"),
+        InlineKeyboardButton("FireFaucet", callback_data="link_clicked"),
+        InlineKeyboardButton("Перейти на сайт FaucetUA", url="https://faucetua.online/")
+    )
 
     bot.send_message(message.chat.id, description, parse_mode="Markdown", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data == "link_clicked")
+def handle_link_click(call):
+    user_id = call.message.chat.id
+    user_clicks[user_id] = user_clicks.get(user_id, 0) + 1
+
+    if user_clicks[user_id] >= 2:
+        bot.answer_callback_query(call.id, "Хочешь больше? Переходи на сайт!", show_alert=True)
+        bot.send_message(user_id, "Хочешь больше? Переходи на сайт https://faucetua.online")
+    else:
+        bot.answer_callback_query(call.id, "Ссылка открыта.")
 
 @bot.message_handler(commands=['sendall'])
 def send_broadcast(message):
